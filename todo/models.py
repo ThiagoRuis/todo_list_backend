7f4a5aca29a3e93be386912b210f5c81d37fac80
John Doe <thiagoruis.mail@gmail.com>
2020-02-26 16:16:52 -0300

    Finalizada API rudimentar

--- todo/models.py
+++ todo/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    deadline = models.DateTimeField()
-    completed_at = models.DateTimeField()
+    deadline = models.DateField()
+    completed_at = models.DateField()
 
     def __str__(self):
         return self.title
---
+++ todo/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        fields = ('id', 'title', 'description', 'deadline', 'completed_at' )
+        model = Todo
